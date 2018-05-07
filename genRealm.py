90be352212979f758e97ad8ca9bf77f6dc952f46
ccsmart <michael@gawati.org>
2018-05-07 11:51:31 +0200

    Add commandline parser

--- genRealm.py
+++ genRealm.py
@@ -2,12 +2,20 @@
 
 import json
 import uuid
+import optparse
 from pprint import pprint
 
-debug=False
 
-realmDNSname='gawati.org'
-realmDisplayName="Gawati"
+optp = optparse.OptionParser()
+optp.add_option('-d', '--debug', dest='debug', help='print debug messages', action='store_true', default=False)
+optp.add_option('-r', '--realm', dest='realm', help='realm ID (DNS domain name recommended)', default='gawati.org')
+optp.add_option('-n', '--name', dest='name', help='verbose realm name (as reference name)', default='Gawati')
+opts, args = optp.parse_args()
+
+debug=opts.debug
+realmDNSname=opts.realm
+realmDisplayName=opts.name
+
 
 UIDmap={'MODEL': realmDNSname}
 UIDnames=['id', 'containerId']
